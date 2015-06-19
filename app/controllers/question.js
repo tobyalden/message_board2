e532f387d9ff60ee61ae12d7d03f550364b0b038
Toby Alden <toby.alden@gmail.com>
2015-06-19 10:31:12 -0700

    Add question editing and deletion.

--- app/controllers/question.js
+++ app/controllers/question.js
@@ -1,12 +1,49 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  actions: {
+  isEditing: false,
+  showAnswerButton: true,
 
-    backToQuestions: function() {
-      this.set('showAnswerButton', true);
-      this.transitionToRoute('questions');
+    actions: {
+
+      editDescription: function() {
+        this.set('isEditing', true);
+      },
+
+      finishEditing: function() {
+        this.set('isEditing', false);
+        var question = this.get('model');
+        question.set('description', this.get('description'));
+        question.save();
+      },
+
+      deleteQuestion: function () {
+       var question = this.get('model');
+       question.deleteRecord();
+       question.save();
+       this.transitionToRoute('questions');
+     },
+
+    //  addAnswer: function() {
+    //    this.set('showAnswerButton', false);
+    //    this.transitionToRoute('answers');
+    //  },
+     //
+    //  deleteAnswer: function(answer) {
+    //    var question = this.get('model');
+    //    question.get('answers').removeObject(answer);
+    //    question.save();
+     //
+    //    answer.deleteRecord();
+    //    answer.save();
+    //    this.transitionToRoute('question', this.get('id'));
+    //  },
+
+     backToQuestions: function() {
+       this.set('showAnswerButton', true);
+       this.transitionToRoute('questions');
+     }
+
     }
 
-  }
 });
